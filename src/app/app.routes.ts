1f1a98c83d9feb5bd5783fa459c58fad1dccfbe5
AhmedElmnshawy <ahmedfartaca4@gmail.com>
2025-02-17 22:52:49 +0200

    edit router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { NoFoundComponent } from './no-found/no-found.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'home',pathMatch:'full'},
-    {path:'home',component:HomeComponent,title:'home'},
-    {path:'about',component:AboutComponent,title:'About'},
-    {path:'contact',component:ContactComponent,title:'Contact'},
-    {path:'portfolio',component:PortfolioComponent,title:'Portfolio'},
-    {path:'**',component:NoFoundComponent,title:'Not-Found'}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, title: 'home' },
+  { path: 'about', component: AboutComponent, title: 'About' },
+  { path: 'contact', component: ContactComponent, title: 'Contact' },
+  { path: 'portfolio', component: PortfolioComponent, title: 'Portfolio' },
+  { path: '**', component: NoFoundComponent, title: 'Not-Found' },
 ];
